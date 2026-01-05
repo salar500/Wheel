6857069ca5359f14e47a3e7041d3073ab8ee92f0
salar500 <baavanandu@gmail.com>
2026-01-05 20:58:30 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,20 @@
+// ======================= sw.js =======================
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("wheel-cache").then(cache =>
+      cache.addAll([
+        "/",
+        "/index.html",
+        "/style.css",
+        "/app.js"
+      ])
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
 
